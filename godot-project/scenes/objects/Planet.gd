e73595e020b737661273ef58e4d9894c0a113330
Benedikt <bruno.xylophon@gmail.com>
2022-04-02 16:03:23 +0200

    planet started

---
+++ godot-project/scenes/objects/Planet.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+onready var sprite=$Sprite
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+const max_hp=1000
+var hp=max_hp;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	receive_damage(20)
+	yield(get_tree().create_timer(1.0),"timeout")
+	receive_damage(400)
+	yield(get_tree().create_timer(1.0),"timeout")
+	receive_damage(1000)
+	yield(get_tree().create_timer(1.0),"timeout")
+	pass # Replace with function body.
+
+func receive_damage(amount):
+	hp=hp-amount;
+	# handle over /underflow
+	if hp < 0:
+		hp=0;
+	if hp > max_hp:
+		hp=max_hp;
+	#load texture according to hp
+	if hp >= 666:
+		sprite.texture = load("res://scenes/objects/planet-good.png")
+	if hp < 666 and hp > 333:
+		sprite.texture = load("res://scenes/objects/planet-medium.png")
+	if hp <= 333:
+		sprite.texture = load("res://scenes/objects/planet-worse.png")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
