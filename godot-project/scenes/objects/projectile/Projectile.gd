13635ae621c4c7848ed9fa91cf18429f61fc418b
Benedikt <bruno.xylophon@gmail.com>
2022-04-03 17:56:56 +0200

    added more projectile types including a homing one

--- godot-project/scenes/objects/projectile/Projectile.gd
+++ godot-project/scenes/objects/projectile/Projectile.gd
@@ -2,16 +2,35 @@ extends Node2D
 var direction
 var damage
 var speed
+var target
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+func start_homing(ptarget):
+			Sprite.texture = load("res://scenes/objects/projectile/projectile-homing.png")
+			target=ptarget
+			damage=10
+			speed=100
+			set_process(true)
 
 func start(pdirection:Vector2, projectile_type:int ):
 	match (projectile_type):
 		1:
-			Sprite.texture = load("res://scenes/objects/projectile/projectile.png")
+			Sprite.texture = load("res://scenes/objects/projectile/projectile-mg.png")
 			direction=pdirection.normalized()
 			damage=10
+			speed=100
+			set_process(true)
+		2:
+			Sprite.texture = load("res://scenes/objects/projectile/projectile-rocket.png")
+			direction=pdirection.normalized()
+			damage=50
+			speed=15
+			set_process(true)
+		3:
+			Sprite.texture = load("res://scenes/objects/projectile/projectile-rocket.png")
+			direction=pdirection.normalized()
+			damage=100
 			speed=100
 			set_process(true)
 		_:
@@ -23,6 +42,8 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
+	if target:
+		direction=(target.position-position).normalized+direction*3
 	position=(position+(direction*speed*delta))
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
